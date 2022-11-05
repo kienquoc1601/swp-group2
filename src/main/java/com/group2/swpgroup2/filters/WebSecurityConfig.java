9ce0cbd514a3db2400af983c03b55497154e3c30
kienquoc1601 <quocbkhe150055@fpt.edu.vn>
2022-11-05 21:38:19 +0700

    dsds

--- src/main/java/com/group2/swpgroup2/filters/WebSecurityConfig.java
+++ src/main/java/com/group2/swpgroup2/filters/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource);
+		
 	}
 
 	@Bean
